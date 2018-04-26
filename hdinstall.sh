70cca27a8a3c655cc5960cca0d93617b62ee0a74
hdcola <huangdong@gmail.com>
2018-04-26 23:24:34 +0800

    fix

--- hdinstall.sh
+++ hdinstall.sh
@@ -1,19 +1,43 @@
 #!/bin/bash
 
+echo "stop install_homeassistant"
+sudo systemctl stop install_homeassistant.service
+
 echo "update pip update source"
 sudo cp ./etc/pip.conf /etc/
 
 echo "update apt update source"
 sudo cp -r ./etc/apt/* /etc/apt/
 
+echo "update hassbian-script"
+sudo hassbian-config upgrade hassbian-script
+
+echo "update hassbian system"
+sudo hassbian-config upgrade hassbian
+
 echo "install libAvahi"
 sudo apt-get -y install libavahi-compat-libdnssd-dev
 
-echo "install homebridge"
-sudo hassbian-config install homebridge
+echo "start install_homeassistant"
+sudo systemctl start install_homeassistant.service
+
+echo "install cloud9"
+sudo hassbian-config install cloud9
 
 echo "update npm update soure"
 sudo npm config set registry https://registry.npm.taobao.org
 
-echo "install cloud9"
-sudo hassbian-config install cloud9
+echo "install homebridge"
+sudo npm install -g --unsafe-perm homebridge
+
+echo "install homebridge-config-ui-x"
+sudo npm install -g --unsafe-perm homebridge-config-ui-x
+
+
+echo "install service"
+sudo cp -r ./etc/systemd/* /etc/systemd/
+sudo systemctl daemon-reload
+sudo systemctl enable homebridge.service
+
+echo "update sudoers"
+sudo cp ./etc/sudoers.d/* /etc/sudoers.d/*
