06d93433e2b52c77276798a0b75b6fd5745ce14a
hdcola <huangdong@gmail.com>
2018-05-06 17:48:23 +0800

    加入frpc

--- bupdate.sh
+++ bupdate.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+wget https://github.com/fatedier/frp/releases/download/v0.18.0/frp_0.18.0_linux_arm.tar.gz
+
+tar xzvf frp_0.18.0_linux_arm.tar.gz
+
+sudo cp frp_0.18.0_linux_arm/frpc /usr/local/bin/
+
+sudo cp -r ./frp /home/homeassistant/
+
+sudo chown -R homeassistant.homeassistant /home/homeassistant/frp
+
 echo "update apt db"
 sudo apt-get update
 
