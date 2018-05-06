da6d21ddb022aa8dca1c5dae0a54eac5bc297689
hdcola <huangdong@gmail.com>
2018-05-06 22:51:05 +0800

    fix

--- zupdate.sh
+++ zupdate.sh
@@ -1,3 +1,7 @@
 sudo hassbian-config upgrade cloud9
 sudo hassbian-config upgrade homeassistant
 sudo hassbian-config upgrade hassbian
+
+sudo systemctl daemon-reload
+sudo systemctl restart home-assistant@homeassistant.service
+sudo systemctl restart cloud9@homeassistant.service
