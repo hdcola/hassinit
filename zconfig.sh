e3dcde81b9f6b7a3e301f36be9fb95e6821dbc93
hdcola <huangdong@gmail.com>
2018-08-11 20:42:03 +0800

    把.homeassistant和.homebridge的.去除，这样方便git推送

--- zconfig.sh
+++ zconfig.sh
@@ -1,12 +1,12 @@
-sudo mkdir /home/homeassistant/.homebridge
-sudo cp -r ./homebridge/* /home/homeassistant/.homebridge/
-sudo chown -R homeassistant.homeassistant /home/homeassistant/.homebridge
+sudo mkdir /home/homeassistant/homebridge
+sudo cp -r ./homebridge/* /home/homeassistant/homebridge/
+sudo chown -R homeassistant.homeassistant /home/homeassistant/homebridge
 
 sudo systemctl restart homebridge.service
 
 
-sudo mkdir /home/homeassistant/.homeassistant
-sudo cp -r ./homeassistant/* /home/homeassistant/.homeassistant/
-sudo chown -R homeassistant.homeassistant /home/homeassistant/.homeassistant
+sudo mkdir /home/homeassistant/homeassistant
+sudo cp -r ./homeassistant/* /home/homeassistant/homeassistant/
+sudo chown -R homeassistant.homeassistant /home/homeassistant/homeassistant
 
 sudo systemctl restart home-assistant@homeassistant.service
