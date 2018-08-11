b972cf11527db2f24c5df3e47f708746220a981c
hdcola <huangdong@gmail.com>
2018-08-12 07:36:38 +0800

    ha的配置完整化

--- zconfig.sh
+++ zconfig.sh
@@ -2,11 +2,10 @@ sudo mkdir /home/homeassistant/homebridge
 sudo cp -r ./homebridge/* /home/homeassistant/homebridge/
 sudo chown -R homeassistant.homeassistant /home/homeassistant/homebridge
 
-sudo systemctl restart homebridge.service
-
-
 sudo mkdir /home/homeassistant/homeassistant
 sudo cp -r ./homeassistant/* /home/homeassistant/homeassistant/
 sudo chown -R homeassistant.homeassistant /home/homeassistant/homeassistant
 
+sudo systemctl daemon-reload
+sudo systemctl restart homebridge.service
 sudo systemctl restart home-assistant@homeassistant.service
