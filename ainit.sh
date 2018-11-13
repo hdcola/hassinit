342c641a2c755da47e7a9aea3e9c5c1071dcc69f
hdcola <huangdong@gmail.com>
2018-11-13 20:27:17 +0800

    适配hassbian1.5.0的权限问题，同时支持ha的0.8以上的版本，使用auth，不再使用http_password

--- ainit.sh
+++ ainit.sh
@@ -10,5 +10,8 @@ sudo cp ./etc/pip.conf /etc/
 echo "update apt update source"
 sudo cp -r ./etc/apt/* /etc/apt/
 
+echo "chown srv/homeassistant to homeassistant"
+sudo chown -R homeassistant:homeassistant /srv/homeassistant
+
 echo "start install_homeassistant"
 sudo systemctl start install_homeassistant.service
